6b02fe07a84e2c2d89b1e77674f213459d897d88
Jason Bland <jtbland@gmail.com>
2018-11-06 20:35:19 -0800

    add force swipe card out of view

--- src/Deck.js
+++ src/Deck.js
@@ -3,6 +3,7 @@ import { Animated, Dimensions, PanResponder, View } from 'react-native';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
+const SWIPE_OUT_DURATION = 250;
 
 class Deck extends Component {
   constructor(props) {
@@ -16,9 +17,11 @@ class Deck extends Component {
       },
       onPanResponderRelease: (event, gesture) => {
         if (gesture.dx > SWIPE_THRESHOLD) {
-          console.log('swiped right');
+          //swiped right
+          this.forceSwipe('right');
         } else if (gesture.dx < -SWIPE_THRESHOLD) {
-          console.log('swiped left');
+          //swiped left
+          this.forceSwipe('left');
         } else {
           this.resetPosition();
         }
@@ -26,6 +29,16 @@ class Deck extends Component {
     });
 
     this.state = { panResponder, position };
+  }
+
+  forceSwipe(direction) {
+    const { position } = this.state;
+    const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH;
+
+    Animated.timing(position, {
+      toValue: { x, y: 0 },
+      duration: SWIPE_OUT_DURATION
+    }).start();
   }
 
   resetPosition() {
