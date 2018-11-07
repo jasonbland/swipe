9fa1d44ac6ce05a8c4d8f869f5ccba04691bb296
Jason Bland <jtbland@gmail.com>
2018-11-06 19:48:05 -0800

    reset card position upon release

--- src/Deck.js
+++ src/Deck.js
@@ -13,10 +13,20 @@ class Deck extends Component {
       onPanResponderMove: (event, gesture) => {
         position.setValue({ x: gesture.dx, y: gesture.dy });
       },
-      onPanResponderRelease: () => {}
+      onPanResponderRelease: () => {
+        this.resetPosition();
+      }
     });
 
     this.state = { panResponder, position };
+  }
+
+  resetPosition() {
+    const { position } = this.state;
+
+    Animated.spring(position, {
+      toValue: { x: 0, y: 0 }
+    }).start();
   }
 
   getCardStyle() {
